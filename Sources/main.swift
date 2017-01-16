9058547dc9186e0fd18fbefa57a24d01d7569253
breeze <breeze@breezedeMBP-2.lan>
2017-01-16 23:25:26 +0800

    first commit

---
+++ Sources/main.swift
@@ -0,0 +1,129 @@
+
+import SwiftProtobuf
+import Foundation
+
+func getLibrary() -> MyLibrary {
+    let map = ["route": "66"]
+    
+    var bookInfo = BookInfo()
+    bookInfo.id = 10
+    bookInfo.title = "Welcome to Swift"
+    bookInfo.author = "Apple Inc2222."
+    
+    var bookInfo2 = BookInfo()
+    bookInfo2.id = 20
+    bookInfo2.title = "ProtoBuf In Swift"
+    bookInfo2.author = "Apple Inc222."
+    
+    var bookInfo3 = BookInfo()
+    bookInfo3.id = 30
+    bookInfo3.title = "Perfect"
+    bookInfo3.author = "PerfectSoft Inc."
+    
+    var bookInfo4 = BookInfo()
+    bookInfo4.id = 40
+    bookInfo4.title = "Go"
+    bookInfo4.author = "Google Inc."
+    
+    var bookInfo5 = BookInfo()
+    bookInfo5.id = 50
+    bookInfo5.title = "React Native"
+    bookInfo5.author = "FaceBook Inc."
+    
+    var bookInfo6 = BookInfo()
+    bookInfo6.id = 60
+    bookInfo6.title = "Java"
+    bookInfo6.author = "Oracle Inc."
+    
+    var bookInfo7 = BookInfo()
+    bookInfo7.id = 40
+    bookInfo7.title = "Go"
+    bookInfo7.author = "Google Inc."
+    
+    var bookInfo8 = BookInfo()
+    bookInfo8.id = 50
+    bookInfo8.title = "React Native"
+    bookInfo8.author = "FaceBook Inc."
+    
+    var bookInfo9 = BookInfo()
+    bookInfo9.id = 60
+    bookInfo9.title = "Java"
+    bookInfo9.author = "Oracle Inc."
+    
+    var bookInfo10 = BookInfo()
+    bookInfo10.id = 40
+    bookInfo10.title = "Go"
+    bookInfo10.author = "Google Inc."
+    
+    var bookInfo11 = BookInfo()
+    bookInfo11.id = 50
+    bookInfo11.title = "React Native"
+    bookInfo11.author = "FaceBook Inc."
+    
+    var bookInfo12 = BookInfo()
+    bookInfo12.id = 60
+    bookInfo12.title = "Java"
+    bookInfo12.author = "Oracle Inc."
+    
+    var library = MyLibrary()
+    library.id = 20
+    library.name = "Swift"
+    library.keys = map
+    library.books = [bookInfo, bookInfo2 , bookInfo3, bookInfo4, bookInfo5, bookInfo6, bookInfo7, bookInfo8, bookInfo9, bookInfo10, bookInfo11,bookInfo12 /**/]
+    
+    return library
+}
+
+// 创建一个 MyLibrary实例
+var protoLibrary = getLibrary()
+var data: Data = try! protoLibrary.serializeProtobuf()
+var str: String = try! protoLibrary.serializeJSON()
+
+
+
+// Protobuf二进制 -> Model
+var startDate = Date()
+var tmpLibrary: MyLibrary?
+for _ in 1...200000 {
+    tmpLibrary = try MyLibrary(protobuf: data)
+}
+var endDate = Date()
+print(try! tmpLibrary?.serializeJSON() ?? "tmpLibrary has not Value")
+print("ProtoBuf Data -> Model Time：\(endDate.timeIntervalSince(startDate))")
+
+
+
+// Protobuf二进制 -> Model
+startDate = Date()
+for _ in 1...200000 {
+    let jsonData = str.data(using: .utf8)
+    let dict:[String: Any]! = try JSONSerialization.jsonObject(with: jsonData!, options: JSONSerialization.ReadingOptions.allowFragments) as? Dictionary<String, Any>
+    
+    tmpLibrary = MyLibrary()
+    let idStr = dict["id"] as! String
+    tmpLibrary?.id = Int64.init(idStr)!
+    tmpLibrary?.name = dict["name"] as! String
+    tmpLibrary?.keys = dict["keys"] as! Dictionary<String, String>
+    
+    var arr: [BookInfo] = [BookInfo]()
+    for bookInfoDict in dict["books"] as! [[String: Any]] {
+        var tmpBookInfo = BookInfo()
+        let bookId = bookInfoDict["id"] as! String
+        tmpBookInfo.id = Int64.init(bookId)!
+        tmpBookInfo.title = bookInfoDict["title"] as! String
+        tmpBookInfo.author = bookInfoDict["author"] as! String
+        arr.append(tmpBookInfo);
+    }
+    tmpLibrary?.books = arr
+    
+}
+endDate = Date();
+print(try! tmpLibrary?.serializeJSON() ?? "tmpLibrary has not Value")
+
+print("JSONSerialization String -> Model Time：\(endDate.timeIntervalSince(startDate))")
+
+
+
+
+
+
